e6f9fb5df09aab1831dea966cf34bb69d96b2b56
YR <resumm.er09@gmail.com>
2023-01-31 17:01:01 +0900

    회원가입하는중 (터지는중)

--- chat_client.py
+++ chat_client.py
@@ -24,15 +24,43 @@ class WindowClass(QMainWindow, form_class):
 
         self.send_btn.clicked.connect(self.send_chat) #ui메시지전송버튼
         self.stackedWidget.setCurrentIndex(0) #스택위젯 0p 고정
-        self.chat_open_btn.clicked.connect(self.chat_open)
+        self.chat_open_btn.clicked.connect(self.chat_open) # 로그인
 
         self.chat_add_btn.clicked.connect(self.create_chatroom) #채팅방 생성 버튼
 
-    def chat_open(self): # 1p 들어가기 버튼 클릭했을때
+    def chat_open(self): # 1p 들어가기 버튼 클릭했을때 - 로그인
         print('button click')
-        self.user_name_label_2p.setText(self.user_name_line_edit_1p.text())
-        self.stackedWidget.setCurrentIndex(1)
-        self.list_up_room() # 채팅방 목록 조회
+        phone = self.user_phone_lineEdit.text()
+        password = self.user_pass_lineEdit.text()
+
+        with self.conn_fetch() as cur:
+            sql = f'SELECT user_name, user_phone, user_pass FROM users WHERE user_phone = "{phone}"'
+            cur.execute(sql)
+            if cur.fetchone() != None:
+                name, id, pw = cur.fetchone()
+            print(name, id, pw)
+
+            # phone 번호 결과가 있으면
+            if id and pw:
+                if password == pw:
+                    print('phone / pass 일치, 로그인 확인')
+                    # self.user_name_label_2p.setText(self.user_name_line_edit_1p.text())
+                    self.user_name_label_2p.setText(name)
+                    self.stackedWidget.setCurrentIndex(1)
+                    self.list_up_room()  # 채팅방 목록 조회
+
+                    # 로그인 경고, 비밀번호 입력란 초기화
+                    self.login_label.setText('')
+                    self.user_pass_lineEdit.text('')
+
+                else:
+                    print('phone / pass 불일치')
+                    self.login_label.setText('휴대폰 번호와 비밀번호를 확인해주세요.')
+
+            elif name == None:
+                print('존재하지 않는 계정')
+                self.login_label.setText('존재하지 않는 계정입니다.')
+
 
 
     def initialize_socket(self):
@@ -90,9 +118,8 @@ class WindowClass(QMainWindow, form_class):
                 sql = 'SELECT * FROM t8_db.room;'  # 채팅방 조회 쿼리
                 cur.execute(sql)
                 rows = cur.fetchall() # 방 목록
-                for room in rows:
-                    print(room)
 
+        # 테이블 행 / 열 설정
         self.chat_list_tableWidget.setColumnCount(2)
         self.chat_list_tableWidget.setRowCount(len(rows))
 
@@ -102,6 +129,7 @@ class WindowClass(QMainWindow, form_class):
             self.chat_list_tableWidget.setItem(col, 1, QTableWidgetItem(str(row[1])))
             col += 1
 
+        # 테이블 헤더 조정
         self.chat_list_tableWidget.setColumnWidth(0, 20)
         self.chat_list_tableWidget.horizontalHeader().setStretchLastSection(True)
 
